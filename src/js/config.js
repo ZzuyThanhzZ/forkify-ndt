4b9e82f5f21d01d2935a847ef5f302051fa528f3
Tenshinhan <ndthanh0206@gmail.com>
2021-12-09 11:33:38 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '303d2d7d-df78-4f72-b48e-8a6473e80ab7';
+export const MODAL_CLOSE_SEC = 2.5;
